d0b3ca481ae1141ec18068b0ad564057a01434d1
Abhishek Kumar <abhishek.andri05@gmail.com>
2024-09-06 16:07:47 +0530

    created backend for signin signup

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import authRoute from './routes/auth.route.js'
+import cookieParser from 'cookie-parser'
+
+dotenv.config()
+const app = express()
+const URL = process.env.CONNECTION_URL
+app.use(express.json())
+app.use(cookieParser())
+mongoose.connect(URL)
+.then(()=>{
+    console.log('connected')
+})
+.catch((e)=>{
+    console.log(e.message)
+})
+app.listen(3000, ()=>{
+    console.log(`Server running on port 3000`)
+})
+
+
+app.use('/api/auth', authRoute)
+app.use((err, req, res, next)=>{
+    const statusCode = err.statusCode || 500
+    const message = err.message || "Internal server error"
+    return res.status(statusCode).json({
+        success:false,
+        message,
+        statusCode
+    })
+})
